f8c92f7dec6229a3d1cf669c08aca00bb8fa1b68
Xiaoqing WANG <158572642+heyisvivian@users.noreply.github.com>
2024-04-09 14:38:21 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,17 @@
-
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import altair as alt
 
 df = pd.read_csv("/Users/vivianwang/Downloads/T2/Data Viz/merged_train.csv")
 
-# Pie chart for alert reason category
-df['alert reason category'].value_counts().plot.pie(autopct='%1.1f%%')
-plt.axis('equal')
-plt.title('Alert Reason Category')
-plt.show()
+# plot the Pie chart for alert reason category using altair
+alert_reason = df['alert_reason_category'].value_counts().reset_index()
+alert_reason.columns = ['alert_reason_category', 'count']
+alert_reason_chart = alt.Chart(alert_reason).mark_bar().encode(
+    x='count',
+    y=alt.Y('alert_reason_category', sort='-x')
+)
+alert_reason_chart
 
-# Pie chart for vehicle type
-df['emergency vehicle type'].value_counts().plot.pie(autopct='%1.1f%%')
-plt.axis('equal')
-plt.title('Emergency Vehicle Type')
-plt.show()
+
 
