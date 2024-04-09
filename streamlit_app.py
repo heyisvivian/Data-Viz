48f57bf2491e433f90205188ab12c846643f0f86
Xiaoqing WANG <158572642+heyisvivian@users.noreply.github.com>
2024-04-09 15:15:36 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import matplotlib.pyplot as plt
 import pandas as pd
 import requests
 from io import StringIO
@@ -24,17 +23,6 @@ merged_data = pd.merge(data_x, data_y, on='emergency vehicle selection')
 
 # Use the merged data in your app
 st.write(merged_data)
-
-#plot the pie chart of emergency vehicle type
-vehicle_type_counts = merged_data['emergency_vehicle_type'].value_counts()
-
-# Create a pie chart
-fig, ax = plt.subplots()
-ax.pie(vehicle_type_counts, labels=vehicle_type_counts.index, autopct='%1.1f%%', startangle=90)
-ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-
-# Display the pie chart using Streamlit
-st.pyplot(fig)
 
 
 
