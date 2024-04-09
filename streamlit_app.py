fbbc5d7dd75d3d5990e301f32127f0614f1e10c9
Xiaoqing WANG <158572642+heyisvivian@users.noreply.github.com>
2024-04-09 15:50:05 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,26 +7,10 @@ import altair as alt
 
 df = pd.read_csv('merged_train_mini.csv')
 
-'''
-@st.cache
-def load_data(url):
-    response = requests.get(url)
-    if response.status_code == 200:
-        data = pd.read_csv(StringIO(response.text))
-        return data
-    else:
-        return pd.DataFrame()
+#plot the pie chart of emergency vehicle type
+df['emergency vehicle type'].value_counts().plot.pie(autopct='%1.1f%%')
+plt.title('Emergency Vehicle Type')
+plt.show()
+st.pyplot()
 
-# URL of your dataset
-data_x = "https://challengedata.ens.fr/participants/challenges/21/download/x-train"
-data_y = "https://challengedata.ens.fr/participants/challenges/21/download/y-train"
-
-# Load data
-data_x = load_data(data_x)
-data_y = load_data(data_y)
-merged_data = pd.merge(data_x, data_y, on='emergency vehicle selection')
-
-# Use the merged data in your app
-st.write(merged_data)
-'''
 
