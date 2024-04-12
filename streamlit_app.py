30edfbf7a8fe560a15299e7e4ab67304eeac51b8
Xiaoqing WANG <158572642+heyisvivian@users.noreply.github.com>
2024-04-12 09:56:05 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-
+import numpy as np
 import pandas as pd
 import streamlit as st
 import matplotlib.pyplot as plt
@@ -7,43 +7,71 @@ from mpl_toolkits.axes_grid1.inset_locator import inset_axes
 
 df = pd.read_csv('merged_train_mini.csv')
 
-'''Emergency Vehicle Response Time Analysis'''
-col1, col2 = st.columns(2)
+# Use HTML with inline CSS to style your title
+st.markdown("""
+    <h1 style='text-align: center; color: red; font-size: 48px;'>
+        Emergency Vehicle Response Time Analysis
+    </h1>""", unsafe_allow_html=True)
 
-#plot the pie chart of emergency vehicle type (top 10) with the color representing the time 
-import matplotlib.pyplot as plt
-import matplotlib.colors as mcolors
-import pandas as pd
+col1, col2 = st.columns(2)
 
 avg_deltas = df.groupby('emergency vehicle type')['delta selection-presentation'].mean()
 top_10_types = df['emergency vehicle type'].value_counts().head(10).index
 top_10_avg_deltas = avg_deltas.loc[top_10_types]
 norm = mcolors.Normalize(vmin=top_10_avg_deltas.min(), vmax=top_10_avg_deltas.max())
 cmap = plt.cm.Reds
-colors = [cmap(norm(value)) for value in top_10_avg_deltas]
+colors = [cmap(norm(value)) for value in top_10_avg_deltas] #assign color 
 fig1, ax1 = plt.subplots()
-explode = [0.05] * 10  
+fig1, ax1 = plt.subplots(figsize=(10, 8))
+explode = [0.05] * 10
+custom_labels = list(top_10_types[:5]) + [None] * (10 - 5)
 patches, texts, autotexts = ax1.pie(
     df['emergency vehicle type'].value_counts().head(10),
-    labels=top_10_types,
+    labels=custom_labels,
     autopct='%1.1f%%',
     startangle=90,
     colors=colors,
     explode=explode,
-    textprops={'fontsize': 9}  
+    textprops={'fontsize': 9}
 )
-for text in texts + autotexts:
-    text.set_color('white')  
-ax1.legend(patches, top_10_types, title="Vehicle Types", loc="center left", bbox_to_anchor=(1, 0, 0.5, 1))
-ax1.set_title('Top 10 Emergency Vehicle Types', fontsize=14)
+for autotext in autotexts:
+    autotext.set_color('black')
+sm = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
+sm.set_array([])
+cbar = plt.colorbar(sm, ax=ax1)
+cbar.set_label('Average Response Time (seconds)')
+ax1.set_title('Top 10 Emergency Vehicle Types by Average Response Time', fontsize=14)
 plt.show()
 with col1:
     st.pyplot(fig1)
     
+     
 #plot the pie chart of alart reason category with color representing the time
-fig2, ax = plt.subplots(figsize=(10, 6))
-df['alert reason category'].value_counts().nlargest(5).plot.pie(autopct='%1.1f%%', ax=ax)
-ax.set_title('Alert Reason Category Top 5')
+avg_deltas_alerts = df.groupby('alert reason category')['delta selection-presentation'].mean()
+top_5_alerts = df['alert reason category'].value_counts().nlargest(5).index
+top_5_avg_deltas_alerts = avg_deltas_alerts.loc[top_5_alerts]
+norm_alerts = mcolors.Normalize(vmin=top_5_avg_deltas_alerts.min(), vmax=top_5_avg_deltas_alerts.max())
+cmap_alerts = plt.cm.Blues 
+colors_alerts = [cmap_alerts(norm_alerts(value)) for value in top_5_avg_deltas_alerts]
+
+fig2, ax2 = plt.subplots(figsize=(10, 8))
+patches, texts, autotexts = ax2.pie(
+    df['alert reason category'].value_counts().nlargest(5),
+    labels=top_5_alerts,
+    autopct='%1.1f%%',
+    startangle=90,
+    colors=colors_alerts
+)
+
+for autotext in autotexts:
+    autotext.set_color('black')
+ax2.set_title('Top 5 Alert Reason Categories by Average Response Time')
+
+sm_alerts = plt.cm.ScalarMappable(cmap=cmap_alerts, norm=norm_alerts)
+sm_alerts.set_array([])
+cbar = plt.colorbar(sm_alerts, ax=ax2)
+cbar.set_label('Average Response Time (seconds)')
+
+plt.show()
 with col2:
     st.pyplot(fig2)
-
